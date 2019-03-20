7d281d03921973dd253f07a7b8fb8f2ffc1f7432
apark105 <andyjpark105@gmail.com>
2019-03-19 21:54:25 -0700

    completed connecting frontend to backend

--- my-app/server/textSearch.js
+++ my-app/server/textSearch.js
@@ -1,28 +1,32 @@
-const express = require('express'); 
+const express = require('express');
 const axios = require('axios');
 const router = express.Router();
 
-module.exports = (app, dB) => { 
-    app.post("/googleTextSearch", (req, res)=>{
-        console.log(req.body)
-        let query = '';
-        let fields = '';
-        let key = '';
-        let location = '';
-        // axios.post("https://maps.googleapis.com/maps/api/place/textsearch/json?",
-        // )
+module.exports = (app, dB) => {
+    app.post("/googleTextSearch", (req, res) => {
+        // console.log('server data:', req.body.data)
+        let query = req.body.data.query;
+        let fields = req.body.data.fields;
+        let key = req.body.data.key;
+        let location = req.body.data.location;
+        axios.get("https://maps.googleapis.com/maps/api/place/textsearch/json?",
+            {
+                params: {
+                    query: query,
+                    fields: fields,
+                    key: key,
+                    location: location,
+                    radius: '30000'
+                }
+            }
+        ).then( (resp) => {
+            
+            // console.log('what is the text Search resp',resp.data.results)
+            res.send(JSON.stringify({ "status": 200, "error": null, "response": resp.data.results }));
+        } )
 
-    })  
-    
-    //  {
-    //     params: {
-    //         query: 'rockclimbing+gym',
-    //         fields: 'formatted_address,name,geometry',
-    //         key: 'AIzaSyD-NNZfs0n53D0caUB0M_ERLC2n9psGZfc',
-    //         location: '33.9316733, -117.8604472'
-    //       }
-    // })
- }  
+    })
+}
 
 
 // router.get( '/', (req, res) =>{
