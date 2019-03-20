230688506c0e016a187f131dfeebe80b3dd9b620
apark105 <andyjpark105@gmail.com>
2019-03-19 19:57:47 -0700

    Completed functionality for sending geolocation to mysql db to retrieve data

--- my-app/server/server.js
+++ my-app/server/server.js
@@ -3,36 +3,42 @@ const express = require('express');
 const cors = require('cors');
 const PORT = process.env.PORT || 3001;
 const address = require('./address');
+var bodyParser = require('body-parser');
+
+const db = require('./db')
 
 const app = express();
 
+// app.use(express.static(__dirname + '/html'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
 app.use(cors());
+// const urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 
-// app.use( (req, res, next) => {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-// });
 
-app.use('/wholeFoods', address);
+// app.post('/wholeFoods/infos', (req, res) =>{
+//     console.log('where the body', req.body.name)
+//     res.send(req.body.name )
+// })
+
+require('./textSearch')(app, db)
+app.use('/wholeFoods', address)
+
+// require('./address')(app, db)
+// app.use('/wholeFoods',urlencodedParser, address);
+
+
+// app.post('/wholeFoods/info', urlencodedParser, (req,resp) => {
+//     console.log('did I get a resp?', req.body)
+
+//     resp.send(JSON.stringify({ "status": 200, "error": null, "response": req.body }))
+// })
 
 app.listen(PORT, () => {
     console.log('Listening to Port:', PORT);
 })
 
 
-// conn.connect( (err) => {
-//   if (err) throw err;
-//   //Select all customers and return the result object:
-//   conn.query("SELECT * FROM c918_AP", (err, result, fields) => {
-//     if (err) throw err;
-//     return result
-//   });
-// });
-
-// // app.use(express.urlencoded)( {extended: false});
-// app.use(express.json());
-// // app.use(express.static());
-
-// app.use(express.static(path.join(__dirname, '/public')));
--- my-app/src/App.js
+++ my-app/src/App.js
@@ -10,11 +10,62 @@ class App extends Component {
 
     this.state = {
       wholeFoods: [],
+      latLng: {}
     }
   }
 
   componentDidMount = () => {
-    axios.get("http://localhost:3001/wholeFoods").then( (resp) => {
+  
+    this.getGoogleSearch();
+    // this.sendLatLngDB();
+
+  }
+
+
+  getLatLng = async (address) => {
+    await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=AIzaSyD-NNZfs0n53D0caUB0M_ERLC2n9psGZfc`).then( (resp) => {
+      console.log('what is the axios', resp.data.results[0].geometry.location)
+      this.setState({
+        latLng: resp.data.results[0].geometry.location,
+      })
+    } ).catch(err => console.log(err))
+
+    this.sendLatLngDB();
+  }
+
+  sendLatLngDB = async () => {
+    const { latLng } = this.state;
+    await axios({
+      "async": true,
+      "crossDomain": true,
+      method: 'post',
+      url: 'http://localhost:3001/wholeFoods/infos',
+      data: latLng,
+      dataType: JSON,
+      headers: {
+          'Content-Type': 'application/json',
+          "cache-control": "no-cache",
+      }
+    }).then( (resp) => {console.log(resp)})
+    this.getWholeFoods();
+  }
+
+  getGoogleSearch =()=>{
+    axios.post("http://localhost:3001/googleTextSearch", 
+      {
+        data: {
+          query: 'rockclimbing+gym',
+          fields: 'formatted_address,name,geometry',
+          key: '',
+          location: '33.9316733, -117.8604472'
+        }, 
+      }
+    ).then( (resp) => {
+      console.log('here is the resp', resp.data.response['lng'])})
+  }
+
+  getWholeFoods = () => {
+  axios.get("http://localhost:3001/wholeFoods").then( (resp) => {
       console.log(resp)
       this.setState({
         wholeFoods:resp.data.response
@@ -22,16 +73,9 @@ class App extends Component {
     } )
   }
 
-
-  getLatLng = (address) => {
-    axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=AIzaSyD-NNZfs0n53D0caUB0M_ERLC2n9psGZfc`).then( (resp) => {
-      debugger;
-      console.log('what is the axios', resp.data.results[0].geometry.location)
-    } ).catch(err => console.log(err))
-  }
-
   render() {
     console.log(this.state.wholeFoods)
+    console.log('what is lat/lng', this.state.latLng);
     return (
       <div className="App">
           <InputInfo submitAddress={this.getLatLng}/>
